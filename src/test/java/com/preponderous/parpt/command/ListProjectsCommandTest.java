622b2e774cfe18c0dfc621a123be52c17eef71bf
dmccoystephenson <dmccoystephenson@gmail.com>
2025-07-13 17:07:50 -0600

    test: remove unused @InjectMocks annotation in ListProjectsCommandTest

--- src/test/java/com/preponderous/parpt/command/ListProjectsCommandTest.java
+++ src/test/java/com/preponderous/parpt/command/ListProjectsCommandTest.java
@@ -4,7 +4,6 @@ import com.preponderous.parpt.repo.ProjectRepository;
 import com.preponderous.parpt.service.ProjectService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
