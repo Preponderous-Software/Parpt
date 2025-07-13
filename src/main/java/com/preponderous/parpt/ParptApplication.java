d3f99b4b5ce40d1822b642b066e5f37955740f5b
BuildTools <unconfigured@null.spigotmc.org>
2025-07-13 10:49:44 -0600

    initial commit

---
+++ src/main/java/com/preponderous/parpt/ParptApplication.java
@@ -0,0 +1,13 @@
+package com.preponderous.parpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/preponderous/parpt/ParptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preponderous.parpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
